534e8de7f8364af23ba781d2d8688d18b590cd0a
benjamin.coder.smith <benjamin.coder.smith@gmail.com>
2008-12-27 01:13:09 +0000

    updated to use harvest system

--- examples/sample.py
+++ examples/sample.py
@@ -8,6 +8,7 @@ except ImportError:
     sys.path.append("../")
 
 from pyctags import exuberant_ctags, ctags_file
+from pyctags.harvests import name_lookup_harvest, by_name_harvest
 import os
 
 source_files = list()
@@ -20,15 +21,18 @@ for (dirpath, dirs, files) in os.walk("../"):
 generator = exuberant_ctags(files=source_files)
 list_o_tags = generator.generate_tags(generator_options={'--fields' : '+n'})
 
-tags = ctags_file(list_o_tags)
+names = name_lookup_harvest()
+by_name = by_name_harvest()
+tagfile = ctags_file(list_o_tags, harvests=[names, by_name])
 
-print "Found %d tags in %d source files." % (len(tags), len(source_files))
+print "Found %d tags in %d source files." % (len(tagfile.tags), len(source_files))
 
 # this fetches unique names
-letter_tags = tags.starts_with('c')
-print "%d of them start with the letter c." % (len(letter_tags))
+letter_tags = names.starts_with('c')
+print "%d individual names start with the letter c." % (len(letter_tags))
+by_name_tags = by_name.retrieve_data()
 
 for t in letter_tags:
     # there can be more than one occurance of a particular name
-    for t2 in tags[t]:
+    for t2 in by_name_tags[t]:
         print "\t%s is in %s on line %s." % (t2.name, t2.file, t2.line_number)
