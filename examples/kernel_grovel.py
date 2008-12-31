5a0c957b6414b70d02abdd0102ec9877d2337446
benjamin.coder.smith <benjamin.coder.smith@gmail.com>
2008-12-31 00:32:44 +0000

    add lgpl preamble to sample files

--- examples/kernel_grovel.py
+++ examples/kernel_grovel.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python
+
+## Copyright (C) 2008 Ben Smith <benjamin.coder.smith@gmail.com>
+
+##    This file is part of pyctags.
+
+##    pyctags is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU Lesser General Public License as published
+##    by the Free Software Foundation, either version 3 of the License, or
+##    (at your option) any later version.
+
+##    pyctags is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+
+##    You should have received a copy of the GNU Lesser General Public License
+##    and the GNU Lesser General Public Licens along with pyctags.  If not, 
+##    see <http://www.gnu.org/licenses/>.
+
 import pyctags, os, time, sys
 
 ## This program can take over six minutes on an AMD Athlon 64 3000+ and 
--- examples/sample.py
+++ examples/sample.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python
+## Copyright (C) 2008 Ben Smith <benjamin.coder.smith@gmail.com>
+
+##    This file is part of pyctags.
+
+##    pyctags is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU Lesser General Public License as published
+##    by the Free Software Foundation, either version 3 of the License, or
+##    (at your option) any later version.
+
+##    pyctags is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+
+##    You should have received a copy of the GNU Lesser General Public License
+##    and the GNU Lesser General Public Licens along with pyctags.  If not, 
+##    see <http://www.gnu.org/licenses/>.
+
+
 import sys
 
 from pyctags import exuberant_ctags, ctags_file
