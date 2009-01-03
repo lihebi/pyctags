491d5da2ed4d634965185dc051b6a0085d49d8d1
benjamin.coder.smith <benjamin.coder.smith@gmail.com>
2009-01-03 18:14:45 +0000

    bump version number for svn

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 import glob
 
 setup(name='pyctags',
-      version='0.5.0',
+      version='0.5.1-svn',
       description='Python ctags interface.',
       author='Ben Smith',
       author_email='benjamin.coder.smith@gmail.com',
